3febb2ec2e6677535f1913ef401f275c832f64e7
GrishaPastukhov <grishafox505@gmail.com>
2025-04-28 15:20:42 +0300

    add: basic app config

---
+++ studyTracker/src/router/Routes.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/home';
+import TestPage from '../pages/TestPage';
+
+
+const AppRoutes = () => {
+    return (
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/test" element={<TestPage />} />
+            </Routes>
+    );
+};
+
+export default AppRoutes;
