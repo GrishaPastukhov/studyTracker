1fb4aba05cdfcd982fe6b42b8a740d4d51eaaacc
GrishaPastukhov <grishafox505@gmail.com>
2025-05-06 17:09:58 +0300

    fix: react roter config

--- studyTracker/src/App.tsx
+++ studyTracker/src/App.tsx
@@ -1,11 +1,41 @@
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
 import './App.css'
-import AppRoutes from './router/Routes'
+import Home from './pages/Home'
+import TestPage from './pages/TestPage'
+
+const Layout = () => {
+  return(
+    <>
+    <header>
+      Header Test 2
+    </header>
+    <Outlet/>
+    <footer>
+      Test footer 2
+    </footer>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {path: 'home', element: <Home/>},
+    ],
+  },
+  {
+    path: '/test',
+    element: <TestPage/>,
+  }
+]);
 
 function App() {
 
   return (
     <>
-    <AppRoutes/>
+    <RouterProvider router={router}/>
     </>
   )
 }
--- studyTracker/src/main.tsx
+++ studyTracker/src/main.tsx
@@ -10,9 +10,7 @@ import { BrowserRouter} from 'react-router-dom'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
     <App />
-    </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
